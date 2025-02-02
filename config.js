9aaf37e76e1b213a9b62350e8eae2d327e136e3f
𝐱𝐂𝐇𝐀𝐌𝐎 🧡⚡ <herokubot458@gmail.com>
2025-02-02 10:29:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
   SESSION_ID: process.env.SESSION_ID || "BHASHI-MD~S2pHVZJA#an6nswNewx-qQlyqwglXhxZ1wlnFEj4oCM5lKItWr-w",
 // For extra thing : BHASHI-MD~(megafileid)
-  MONGODB: process.env.MONGODB || "mongodb+srv://herokubot458:QAT5sKzU9JnlXJAS@cluster0.vdcmi.mongodb.net/",
+  MONGODB: process.env.MONGODB || "mongodb+srv://herokubot458:QAT5sKzU9JnlXJAS@cluster0.u0w6u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 
     
  
